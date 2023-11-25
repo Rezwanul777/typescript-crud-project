7f8a24caa25d11301cb533aff8de98aedcb033d1
unknown <rezwanul.haquebd777gmail.com>
2023-11-25 23:40:43 +0600

    user interface and schema crested

---
+++ src/app/config/modules/user/user.interface.ts
@@ -0,0 +1,30 @@
+import { Model } from 'mongoose';
+
+export interface TUser {
+    userId: number;
+    username: string;
+    password: string;
+    fullName: {
+      firstName: string;
+      lastName: string;
+    };
+    age: number;
+    email: string;
+    isActive: boolean;
+    hobbies: string[];
+    address: {
+      street: string;
+      city: string;
+      country: string;
+    };
+    orders: Array<{
+      productName: string;
+      price: number;
+      quantity: number;
+    }>;
+}
+
+export type UserModel = Model<
+TUser
+  
+>;
---
+++ src/app/config/modules/user/user.model.ts
@@ -0,0 +1,33 @@
+import { Schema, model } from "mongoose";
+import { TUser, UserModel } from "./user.interface";
+
+
+const userSchema = new Schema<TUser>({
+    userId: { type: Number, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    fullName: {
+      firstName: { type: String, required: true },
+      lastName: { type: String, required: true },
+    },
+    age: { type: Number, required: true },
+    email: { type: String, required: true },
+    isActive: { type: Boolean, default: true },
+    hobbies: [{ type: String }],
+    address: {
+      street: { type: String },
+      city: { type: String },
+      country: { type: String },
+    },
+    orders: [
+      {
+        productName: { type: String },
+        price: { type: Number },
+        quantity: { type: Number },
+      },
+    ],
+  });
+  
+  
+  
+  export const User = model<TUser, UserModel>('Student', userSchema);
