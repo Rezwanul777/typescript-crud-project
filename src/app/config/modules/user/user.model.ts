4647fb4b1c1e50e211c80eb83b0aafabc4b7a596
unknown <rezwanul.haquebd777gmail.com>
2023-11-26 20:13:22 +0600

    password hash

--- src/app/config/modules/user/user.model.ts
+++ src/app/config/modules/user/user.model.ts
@@ -1,39 +1,70 @@
-import { Schema, model } from "mongoose";
-import { TUser, UserModel } from "./user.interface";
+import { Schema, model } from 'mongoose'
+import { TUser, UserModel } from './user.interface'
+import config from '../../../config';
+import bcrypt from 'bcrypt'
 
 
 const userSchema = new Schema<TUser>({
-    userId: { type: Number, required: true, unique: true },
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true, maxlength:[20,'Password cannot be more than 20'] },
-    fullName: {
-      firstName: { type: String, required:[true, 'First Name is required'],  maxlength: [20, 'First name cannot be more than 20'],
-      trim: true },
-      lastName: { type: String, required:[true, 'Last Name is required'],  maxlength: [20, 'Last name cannot be more than 20'],
-      trim: true  },
+  userId: { type: Number, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  password: {
+    type: String,
+    required: true,
+    maxlength: [20, 'Password cannot be more than 20'],
+  },
+  fullName: {
+    firstName: {
+      type: String,
+      required: [true, 'First Name is required'],
+      maxlength: [20, 'First name cannot be more than 20'],
+      trim: true,
     },
-    age: { type: Number, required: true },
-    email: { type: String, required: true },
-    isActive: { type: Boolean, default: true },
-    hobbies: [{ type: String }],
-    address: {
-      street: { type: String },
-      city: { type: String },
-      country: { type: String },
+    lastName: {
+      type: String,
+      required: [true, 'Last Name is required'],
+      maxlength: [20, 'Last name cannot be more than 20'],
+      trim: true,
     },
-    isDeleted:{
-      type: Boolean,
-      default: false,
+  },
+  age: { type: Number, required: true },
+  email: { type: String, required: true },
+  isActive: { type: Boolean, default: true },
+  hobbies: [{ type: String }],
+  address: {
+    street: { type: String },
+    city: { type: String },
+    country: { type: String },
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+  orders: [
+    {
+      productName: { type: String },
+      price: { type: Number },
+      quantity: { type: Number },
     },
-    orders: [
-      {
-        productName: { type: String },
-        price: { type: Number },
-        quantity: { type: Number },
-      },
-    ],
-  });
-  
-  
-  
-  export const User = model<TUser, UserModel>('User', userSchema);
+  ],
+})
+
+// pre save middleware/hook, will work on create() save
+
+userSchema.pre('save', async function (next) {
+  // hashing password saved
+  // eslint-disable-next-line @typescript-eslint/no-this-alias
+  const user = this
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_round),
+  )
+  next()
+})
+
+// post save middleware/hook, 
+userSchema.post('save', function (doc,next) {
+  doc.password='';
+  next()
+ });
+
+export const User = model<TUser, UserModel>('User', userSchema)
